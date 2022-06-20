3703ed22f460792694263a980376cfe2587ae2b1
winston kiprop <winstonekip50@gmail.com>
2022-06-20 09:50:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+const submitData = (username,useremail)=>{
+	return fetch('http://localhost:3000/users',{
+		 method:'POST',
+		 headers:{
+			 'content-Type':'application/json',
+			 Accept:'application/json'
+		 },
+		 body:JSON.stringify({
+			 name:username,
+			 email:useremail
+		 })
+	 })
+	 .then((response)=>response.json())
+	 .then((data)=>{
+		 let body = document.querySelector('body')
+		 body.innerHTML = data.id
+	  console.log(data)
+	 })
+	 .catch(()=>{
+		 document.querySelector('body').innerHTML = 'Unauthorized Access'
+	 })
+  } 
